c6e6dd5950e4c0644b069cca6db1e565a0b53a73
omarmalik710 <omarmalik710@gmail.com>
2021-03-06 00:01:04 +0100

    Attempted block Cholesky decomp but it didn't work yet. Must revise.

--- main.c
+++ main.c
@@ -6,41 +6,38 @@
 int main() {
     double time1, time2;
 
-    int size = 8;
-    int blockSize = 2;
+    int size = 2000;
+    int blockSize = 16;
     if (size%blockSize != 0) {
         printf("[ERROR] Matrix size %dx%d not divisible by block size %dx%d!\n", size,size, blockSize,blockSize);
         exit(1);
     }
 
     double *A = randHerm(size);
-    printf("A =\n");
-    printMatrix(A, size);
-    putchar('\n');
+    //printf("A =\n");
+    //printMatrix(A, size);
+    //putchar('\n');
 
     //LD_pair LD = cholDecomp_LD_blocks(A, size, blockSize);
     LD_pair LD = cholDecomp_LD(A, size);
     double *LxD = matMulDiag(LD.L, LD.D, size);
-    time1 = get_wall_seconds();
     double *LT = transpose_blocks(LD.L, size, blockSize);
     //double *LT = transpose(LD.L, size);
-    time2 = get_wall_seconds();
     double *LxDxLT = matMul(LxD,LT, size);
 
-    printf("L =\n");
-    printMatrix(LD.L, size);
-    putchar('\n');
+    //printf("L =\n");
+    //printMatrix(LD.L, size);
+    //putchar('\n');
 
-    printf("D = \n");
-    printArray(LD.D, size);
+    //printf("D = \n");
+    //printArray(LD.D, size);
 
-    printf("L*D*LT =\n");
-    printMatrix(LxDxLT, size);
+    //printf("L*D*LT =\n");
+    //printMatrix(LxDxLT, size);
 
     if (matEqual(A, LxDxLT, size, 1e-12)) {
         printf("A = L*D*LT :D\n");
     } else { printf("A != L*D*LT :(\n"); }
-    printf("[TIME] Matrix transpose took %lf seconds.\n", time2-time1);
 
     free(A);
     free(LD.L);
